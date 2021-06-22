012ad49a33192909f463df7e99575b90b4ccaf60
Nilesh Sinha <nilesh.sinha@ymedialabs.com>
2021-06-22 17:47:10 +0530

    add loader

--- helpers/string.ts
+++ helpers/string.ts
@@ -10,7 +10,7 @@ export const decodeEscapeString = (str) =>
   str
     .replace(/&amp;/g, '&')
     .replace(/&lt;/g, '<')
-    .replace(/'&gt;'/g, '>')
+    .replace(/&gt;/g, '>')
     .replace(/&quot;/g, '"')
     .replace(/&#039;/g, `'`)
     .replace(/&Aring;/g, 'Å')
