5f7892d02855dcd734154ffc6db22b85cca3c27d
Nilesh Sinha <nilesh.sinha@ymedialabs.com>
2021-06-22 11:02:36 +0530

    add quiz questions api call

--- pages/quiz/[qid].tsx
+++ pages/quiz/[qid].tsx
@@ -1,11 +1,52 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 import { useRouter } from 'next/router';
-import React, { FC } from 'react';
+import React from 'react';
+import Head from 'next/head';
+import { NextPage } from 'next';
+import { QuizPageProps } from '../../types/quizPageProps';
 
-const Quiz: FC = () => {
+const Quiz: NextPage<QuizPageProps> = ({ quizInfo }: QuizPageProps) => {
   const router = useRouter();
   const { qid } = router.query;
+  return (
+    <main className="container">
+      <Head>
+        <title>Quiz| {quizInfo[0].category}</title>
+      </Head>
+      <div className="quiz-title">
+        <span>Quiz on {quizInfo[0].category}</span>
+      </div>
+      <style jsx lang="scss">
+        {`
+          .quiz-title {
+            width: 100%;
+            box-sizing: border-box;
+            padding: 0 15px;
+          }
+          .quiz-title span {
+            font-size: 2rem;
+          }
+          .trivia-category-section {
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            width: 100%;
+          }
+        `}
+      </style>
+    </main>
+  );
+};
 
-  return <div>Quiz: {qid}</div>;
+Quiz.getInitialProps = async ({ query }) => {
+  const response = await fetch(
+    `https://opentdb.com/api.php?amount=10&category=${query.qid}`
+  );
+  const jsonResponse = await response.json();
+  return {
+    quizInfo: jsonResponse.results,
+  };
 };
 
 export default Quiz;
---
+++ types/quizPageProps.ts
@@ -0,0 +1,12 @@
+export type QuizQuestion = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: Array<string>;
+  question: string;
+  type: string;
+};
+
+export type QuizPageProps = {
+  quizInfo: QuizQuestion[];
+};
